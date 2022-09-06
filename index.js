62847083acfe3d1d1b1b6b8a6e503909b97811e6
Vinícius Soares <vinivsp2304@hotmail.com>
2022-09-06 16:46:36 -0300

    add: rota get by id

--- index.js
+++ index.js
@@ -29,10 +29,7 @@ app.post("/save", async (req, res) => {
 
   for (const field of reqFields) {
     if (!req.body[field]) {
-      return res.status(400).json({
-        causa: "Nenhum dos campos podem ser vazios",
-        solucao: "Preencha todos os campos corretamente",
-      });
+      return res.status(400).json({ status_400 });
     }
   }
 
@@ -54,3 +51,27 @@ app.get("/findall", async (req, res) => {
 
   res.send(response);
 });
+
+app.get("/findone/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const response = await User.findOne({
+    where: { id },
+  });
+  if (response != undefined) {
+    return res.send(response);
+  }
+  res.status(404).json({ status_404 });
+});
+
+/* ### STATUS HTTP ### */
+
+const status_400 = {
+  err: "O id passado é do tipo texto.",
+  Payload: "O id deve conter apenas número(s).",
+};
+
+const status_404 = {
+  err: "O id inserido não foi encontrado.",
+  Payload: "Insira um id válido para a busca.",
+};
